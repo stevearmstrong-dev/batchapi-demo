4c8f564d6fcd0e2fcab6540b4c441f62d2fa01b0
W0474997SteveArmstrong <113034949+W0474997SteveArmstrong@users.noreply.github.com>
2024-07-05 09:28:56 -0300

    Initial commit: Add main Spring Boot application class

---
+++ src/main/java/com/example/batchapidemo/BatchApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.batchapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApiDemoApplication.class, args);
+    }
+
+}
